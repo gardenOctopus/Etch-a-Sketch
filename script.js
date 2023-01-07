7702f10ac870a967111cda2b93bf3861003bfa54
gardenOctopus <zebraspider@protonmail.com>
2023-01-07 16:57:13 -0500

    Created javascript grid

---
+++ script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector('#container');
+
+//Creates Cell Grid
+function makeCells(col, rows) {
+        for (i = 0; i < (col * rows); i++) {
+            let cell = document.createElement('div');
+            container.appendChild(cell);
+            cell.setAttribute('style', 'border-style: solid; border-color: black; border-width: 1px; width: 25px; height: 25px;');
+        }
+
+}
+
+makeCells(16, 16);
