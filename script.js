76a3a0212d7867337548f137ce1b176dc5ee6ef0
gardenOctopus <zebraspider@protonmail.com>
2023-01-08 19:23:16 -0500

    Made Colour Dynamic && Changed Border Styles

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const container = document.querySelector('#container');
 
 //Creates Cell Grid
-function makeCells(col, rows) {
+function makeCells(col, rows, e) {
+        //Creates Cells
         for (i = 0; i < (col * rows); i++) {
             let cell = document.createElement('div');
             container.appendChild(cell);
-            cell.setAttribute('style', 'border-style: solid; border-color: black; border-width: 1px; width: 25px; height: 25px;');
-            cell.addEventListener('mouseover', () => hoverColour());
-            function hoverColour() {
-                cell.style.backgroundColor = 'blue';
+            //Styles Cells
+            cell.setAttribute('style', 'border-style: solid; border-color: #507b9c; border-width: 1px; width: 25px; height: 25px;');
+            //Sets Hover Event
+            cell.addEventListener('mouseover', () => hoverColour(e));
+            function hoverColour(e) {
+                cell.style.backgroundColor = e;
             }
         }
 }
 
-makeCells(16, 16);
+makeCells(16, 16, '#0000ff');
