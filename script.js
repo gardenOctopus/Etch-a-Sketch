59555617224413c420df516fbff6979634ea8d96
gardenOctopus <zebraspider@protonmail.com>
2023-01-08 19:06:04 -0500

    Created hover effect

--- script.js
+++ script.js
@@ -6,8 +6,11 @@ function makeCells(col, rows) {
             let cell = document.createElement('div');
             container.appendChild(cell);
             cell.setAttribute('style', 'border-style: solid; border-color: black; border-width: 1px; width: 25px; height: 25px;');
+            cell.addEventListener('mouseover', () => hoverColour());
+            function hoverColour() {
+                cell.style.backgroundColor = 'blue';
+            }
         }
-
 }
 
 makeCells(16, 16);
